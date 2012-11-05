4c0bc1294989d6662056ce1900560dd2894f7932
chad <chad@c030bedf-b1ec-4d7f-af68-6bc22fd3d7e3>
2012-11-05 00:11:16 +0000

    Updated changelog and improved png resolution

--- savePNG.php
+++ savePNG.php
@@ -11,7 +11,7 @@ $fp = fopen($file . ".svg", "w") or die("Couldn't open $file for writing!");
 fwrite($fp, $data) or die("Couldn't write values to file!");
 fclose($fp);
 echo "Saved to $file successfully!";
-system("inkscape -z --file=" . $file . ".svg  --export-png=" . $file . ".png");
+system("inkscape -z --file=" . $file . ".svg  --export-png=" . $file . ".png" . " --export-width=5000  --export-height=6471");
 ob_clean();
 flush();
 sleep(3);
