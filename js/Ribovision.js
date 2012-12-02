bf67e56f2414bb3099c8e6da27e4fd78e294b936
lanwang <lanwang@c030bedf-b1ec-4d7f-af68-6bc22fd3d7e3>
2012-12-02 17:38:00 +0000

    add draw line function

--- js/Ribovision.js
+++ js/Ribovision.js
@@ -3685,18 +3685,92 @@ function views_proportion_change(leftPercentage, rightPercentage){
 }	
 
 ////////////////Nav Line ///////
-function drawNavLine(){
-	var navLineData = [],
-	navLineWidth = 400,
-	navLineHeight = 200,
-	margin = 20;
-	
-	var yPosition = d3.scale.linear().domain([0, d3.max(navLineData)]).range([0 + margin, navLineHeight - margin]);
-	var xPosition = d3.scale.linear().domain([0, navLineData.length]).range([0 + margin, navLineWidth - margin]);
-	
-	for (var i =0; i<rvDataSets[0].Residues.length;i++){
+/**
+ *for (var i =0; i<rvDataSets[0].Residues.length;i++){
 		var newNumber = rvDataSets[0].Residues[i].mean_tempFactor;
         navLineData = navLineData.concat(newNumber);
 	}
 	console.log(navLineData);
+ *  */
+
+function drawNavLine(){
+	
+		
+		var data = [];
+		
+		for (var i =0; i<rvDataSets[0].Residues.length;i++){
+		var newNumber = rvDataSets[0].Residues[i].mean_tempFactor;
+        data = data.concat(newNumber);
+		}
+		console.log(data);
+		
+		var	w = 400,
+			h = 200,
+			margin = 20,
+			y = d3.scale.linear().domain([0, d3.max(data)]).range([0 + margin, h - margin]),
+			x = d3.scale.linear().domain([0, data.length]).range([0 + margin, w - margin])
+
+			var vis = d3.select("#NavLineDiv")
+			    .append("svg:svg")
+			    .attr("width", w)
+			    .attr("height", h)
+
+			var g = vis.append("svg:g")
+			    .attr("transform", "translate(0, 200)");
+			
+			var line = d3.svg.line()
+			    .x(function(d,i) { return x(i); })
+			    .y(function(d) { return -1 * y(d); })
+			
+			g.append("svg:path").attr("d", line(data));
+			
+			g.append("svg:line")
+			    .attr("x1", x(0))
+			    .attr("y1", -1 * y(0))
+			    .attr("x2", x(w))
+			    .attr("y2", -1 * y(0))
+
+			g.append("svg:line")
+			    .attr("x1", x(0))
+			    .attr("y1", -1 * y(0))
+			    .attr("x2", x(0))
+			    .attr("y2", -1 * y(d3.max(data)))
+			
+			g.selectAll(".xLabel")
+			    .data(x.ticks(5))
+			    .enter().append("svg:text")
+			    .attr("class", "xLabel")
+			    .text(String)
+			    .attr("x", function(d) { return x(d) })
+			    .attr("y", 0)
+			    .attr("text-anchor", "middle")
+
+			g.selectAll(".yLabel")
+			    .data(y.ticks(4))
+			    .enter().append("svg:text")
+			    .attr("class", "yLabel")
+			    .text(String)
+			    .attr("x", 0)
+			    .attr("y", function(d) { return -1 * y(d) })
+			    .attr("text-anchor", "right")
+			    .attr("dy", 4)
+			
+			g.selectAll(".xTicks")
+			    .data(x.ticks(5))
+			    .enter().append("svg:line")
+			    .attr("class", "xTicks")
+			    .attr("x1", function(d) { return x(d); })
+			    .attr("y1", -1 * y(0))
+			    .attr("x2", function(d) { return x(d); })
+			    .attr("y2", -1 * y(-0.3))
+
+			g.selectAll(".yTicks")
+			    .data(y.ticks(4))
+			    .enter().append("svg:line")
+			    .attr("class", "yTicks")
+			    .attr("y1", function(d) { return -1 * y(d); })
+			    .attr("x1", x(-0.3))
+			    .attr("y2", function(d) { return -1 * y(d); })
+			    .attr("x2", x(0))
+		
 }
