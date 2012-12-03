b5037c5e352ac3417a355416bad5e32fc900aad7
lanwang <lanwang@c030bedf-b1ec-4d7f-af68-6bc22fd3d7e3>
2012-12-03 19:57:50 +0000

    update popup window style

--- js/Ribovision.js
+++ js/Ribovision.js
@@ -4030,7 +4030,7 @@ d3.csv("EC_LSU_Struct_ConservationTable.csv", function(csv) {
 			   		return h - (d * 4)-4 ;
 			   })
 			   .attr("font-family", "sans-serif")
-			   .attr("font-size", "8px")
+			   .attr("font-size", "9px")
 			   .attr("fill", "black")
 			   .attr('class','number')
 			   .text(String);
