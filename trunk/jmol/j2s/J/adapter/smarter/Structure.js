9f681561f561b30727e1497d663d41a59df2f95b
chad <chad@c030bedf-b1ec-4d7f-af68-6bc22fd3d7e3>
2014-05-20 18:39:52 +0000

    Bump Version Number. Update JSmol.

--- trunk/jmol/j2s/J/adapter/smarter/Structure.js
+++ trunk/jmol/j2s/J/adapter/smarter/Structure.js
@@ -1,5 +1,5 @@
 Clazz.declarePackage ("J.adapter.smarter");
-Clazz.load (null, "J.adapter.smarter.Structure", ["J.constant.EnumStructure"], function () {
+Clazz.load (null, "J.adapter.smarter.Structure", ["J.c.STR"], function () {
 c$ = Clazz.decorateAsClass (function () {
 this.structureType = null;
 this.substructureType = null;
@@ -22,17 +22,17 @@ Clazz.prepareFields (c$, function () {
 this.atomStartEnd =  Clazz.newIntArray (2, 0);
 this.modelStartEnd = [-1, -1];
 });
-c$.getHelixType = $_M(c$, "getHelixType", 
+c$.getHelixType = Clazz.defineMethod (c$, "getHelixType", 
 function (type) {
 switch (type) {
 case 1:
-return J.constant.EnumStructure.HELIXALPHA;
+return J.c.STR.HELIXALPHA;
 case 3:
-return J.constant.EnumStructure.HELIXPI;
+return J.c.STR.HELIXPI;
 case 5:
-return J.constant.EnumStructure.HELIX310;
+return J.c.STR.HELIX310;
 }
-return J.constant.EnumStructure.HELIX;
+return J.c.STR.HELIX;
 }, "~N");
 Clazz.makeConstructor (c$, 
 function (modelIndex, structureType, substructureType, structureID, serialID, strandCount) {
@@ -43,8 +43,8 @@ this.setModels (modelIndex, 0);
 this.structureID = structureID;
 this.strandCount = strandCount;
 this.serialID = serialID;
-}, "~N,J.constant.EnumStructure,J.constant.EnumStructure,~S,~N,~N");
-$_M(c$, "set", 
+}, "~N,J.c.STR,J.c.STR,~S,~N,~N");
+Clazz.defineMethod (c$, "set", 
 function (startChainID, startSequenceNumber, startInsertionCode, endChainID, endSequenceNumber, endInsertionCode, istart, iend) {
 this.startChainID = startChainID;
 this.startSequenceNumber = startSequenceNumber;
@@ -55,7 +55,7 @@ this.endInsertionCode = endInsertionCode;
 this.atomStartEnd[0] = istart;
 this.atomStartEnd[1] = iend;
 }, "~N,~N,~S,~N,~N,~S,~N,~N");
-$_M(c$, "setModels", 
+Clazz.defineMethod (c$, "setModels", 
 function (model1, model2) {
 this.modelStartEnd[0] = model1;
 this.modelStartEnd[1] = (model2 == 0 ? model1 : model2);
--- trunk/jmol/j2s/JU/T4.js
+++ trunk/jmol/j2s/JU/T4.js
@@ -1,41 +1,41 @@
 Clazz.declarePackage ("JU");
-Clazz.load (null, "JU.T4", ["JU.T3"], function () {
+Clazz.load (["JU.T3"], "JU.T4", null, function () {
 c$ = Clazz.decorateAsClass (function () {
-this.x = 0;
-this.y = 0;
-this.z = 0;
 this.w = 0;
 Clazz.instantialize (this, arguments);
-}, JU, "T4", null, java.io.Serializable);
+}, JU, "T4", JU.T3);
 Clazz.makeConstructor (c$, 
 function () {
+Clazz.superConstructor (this, JU.T4, []);
 });
-$_M(c$, "set", 
+Clazz.defineMethod (c$, "set4", 
 function (x, y, z, w) {
 this.x = x;
 this.y = y;
 this.z = z;
 this.w = w;
 }, "~N,~N,~N,~N");
-$_M(c$, "scale", 
+Clazz.defineMethod (c$, "scale4", 
 function (s) {
-this.x *= s;
-this.y *= s;
-this.z *= s;
+this.scale (s);
 this.w *= s;
 }, "~N");
-$_V(c$, "hashCode", 
+Clazz.overrideMethod (c$, "hashCode", 
 function () {
 return JU.T3.floatToIntBits0 (this.x) ^ JU.T3.floatToIntBits0 (this.y) ^ JU.T3.floatToIntBits0 (this.z) ^ JU.T3.floatToIntBits0 (this.w);
 });
-$_V(c$, "equals", 
+Clazz.overrideMethod (c$, "equals", 
 function (o) {
 if (!(Clazz.instanceOf (o, JU.T4))) return false;
 var t = o;
 return (this.x == t.x && this.y == t.y && this.z == t.z && this.w == t.w);
 }, "~O");
-$_V(c$, "toString", 
+Clazz.overrideMethod (c$, "toString", 
 function () {
 return "(" + this.x + ", " + this.y + ", " + this.z + ", " + this.w + ")";
+});
+Clazz.overrideMethod (c$, "toJSON", 
+function () {
+return "[" + this.x + ", " + this.y + ", " + this.z + ", " + this.w + "]";
 });
 });
