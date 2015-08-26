2fa5e0d7f10de9ea3adeb91684220fa9732f1bcf
Kaley Findley <kfindley7@gatech.edu>
2015-08-26 17:00:17 -0400

    Changed dbname
    Changed db name to RiboVisionDevel2

--- getData.php
+++ getData.php
@@ -5,7 +5,7 @@ header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
 
-$dsn = "mysql:host=localhost;dbname=Ribosome_View2;charset=utf8";
+$dsn = "mysql:host=localhost;dbname=RiboVisionDevel2;charset=utf8";
 $username = "website";
 $pdo = new PDO($dsn, $username);
 
